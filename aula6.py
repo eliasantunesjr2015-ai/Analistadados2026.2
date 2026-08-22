80142c5b093c1f775fcfa3fa397b6b8b34f3a799
Elias jr <eliasantunesjr2015@gmail.com>
2026-08-22 00:50:40 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,57 @@
+
+### ESTRUTURAS DE REPETIÇÃO:
+
+# FOR:
+# for i in range(-102,-1,2):
+#   print(i)
+
+# for ano in range(2026, 2043, 4):
+#     print(ano)
+
+
+# while
+
+# somador = int(input("Registro:"))
+# controle = 0
+
+# while controle <= 30:
+#     controle=controle+somador
+#     somador = int(input("Registro:"))
+
+# print("Oficina lotada!")
+
+
+
+
+
+
+# FOR 
+acertou = 0
+while acertou < 5:
+    print(f"Número {acertou + 1} de 5:") 
+    num = float(input("Digite um número: ")) 
+        
+    dobro = num * 2 
+    triplo = num * 3 
+    quádruplo = num * 4 
+        
+    print(f"  Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quádruplo}\n")
+    acertou+=1 
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
