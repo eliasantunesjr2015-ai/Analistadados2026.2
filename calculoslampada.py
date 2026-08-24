886bf82d02ead1c023b40277322fc30416a1397a
Elias jr <eliasantunesjr2015@gmail.com>
2026-08-24 12:26:50 -0300

    Update calculoslampada.py

--- calculoslampada.py
+++ calculoslampada.py
@@ -38,34 +38,9 @@ print(f"Total de lâmpadas que devem ser instaladas: {unidades}")
 
 # O programa recebe a potência da lâmpada, a largura e o comprimento através do comando input().
 
-#capacidade_lampada = int(input("Qual a potência de cada lâmpada (W)? "))
-#medida_largura = int(input("Digite a largura da peça (m): "))
-#medida_comprimento = int(input("Digite o comprimento da peça (m): "))
-
 #Processamento: Ele multiplica largura por comprimento para achar a área (total_m2) e multiplica o resultado por 3 para achar os Watts totais (carga_necessaria).
 
-#total_m2 = medida_largura * medida_comprimento
-#carga_necessaria = total_m2 * CONSUMO_M2
-
-#print("Espaço total medido:", total_m2, "m².")
-#print("Necessidade total de iluminação:", carga_necessaria, "Watts.")
-
 #(if/elif/else): Ele testa se a carga total cabe em 1 lâmpada, 2 lâmpadas, 3 lâmpadas (e assim por diante), multiplicando a potência individual até o limite de 6 unidades.
-
-#if carga_necessaria <= capacidade_lampada:
-    #unidades = 1
-#elif carga_necessaria <= (capacidade_lampada * 2):
-    #unidades = 2
-#elif carga_necessaria <= (capacidade_lampada * 3):
-    #unidades = 3
-#elif carga_necessaria <= (capacidade_lampada * 4):
-    #unidades = 4
-#elif carga_necessaria <= (capacidade_lampada * 5):
-    #unidades = 5
-#elif carga_necessaria <= (capacidade_lampada * 6):
-    #unidades = 6
-#else:
-    #unidades = "Acima de 6 unidades (ambiente excessivamente grande)"
 
 # O programa exibe na tela o número final de lâmpadas guardado na variável unidades.
 
